11bcea209f08affba7d8712d5de66e65c68b281a
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-09-26 18:08:26 +0300

    Done.

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,18 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name], methods: [:summary]
+    # Only for including only the ones you need and except removes those specified from being viewed.
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese
+    else
+      render json: { error: "Cheese not found" }, status: 404 # :not_found
+    end
   end
 
 end
